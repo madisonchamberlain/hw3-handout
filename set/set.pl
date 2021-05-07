151442ccb3ebc7d441478ce0e667b68e3504c803
mpchambe <mpchambe@ad3.ucdavis.edu@pc15.cs.ucdavis.edu>
2021-05-07 09:23:31 -0700

    set.pl has 100% pass but only 40% coverage

--- set/set.pl
+++ set/set.pl
@@ -1,11 +1,40 @@
-isUnion(Set1,Set2,Union) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+/* union if all sets empty */
+isUnion([],[],[]).
 
-isIntersection(Set1,Set2,Intersection) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+/* union if one set empty, and other set = union */
+isUnion(Set1, [], Set1).
 
-isEqual(Set1,Set2) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+/* if the head of set2 = head of union, and head is not in the other set, recurse on the tail */
+isUnion(Set1, [H2|T2], [H2|Union]) :-
+not(member(H2, Set1)), isUnion(Set1, T2, Union).
+
+/* if h2 is a member of set1, recurse on all of union, not just the tail */
+isUnion(Set1, [H2|T2], Union) :-
+member(H2, Set1), isUnion(Set1, T2, Union).
+
+
+
+
+
+/* intersection if all sets empty */
+isIntersection([], Set2, []).
+
+/* if the first item of Set1 in Set2, and is first item of intersection */
+/*recurse on tail of set1 and tail of set2 */
+isIntersection([H1|T1], Set2, [H1|Intersection]) :-
+member(H1, Set2), isIntersection(T1, Set2, Intersection).
+
+/* if the first item of Set1 not in set2, only recurse on the tail of Set1 */
+isIntersection([H1|T1], Set2, Intersection) :-
+not(member(H1, Set2)), isIntersection(T1, Set2, Intersection).
+
+
+
+
+/* equal if both are blank */
+isEqual([], []).
+
+/* if set2 without H1 isEqual to tail1 */
+isEqual([H1|T1], Set2) :-
+    select(H1, Set2, Short2),
+    isEqual(T1, Short2).
