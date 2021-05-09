d46a616dc42103519c78aa2f4daf18fad833df0e
mpchambe <mpchambe@ad3.ucdavis.edu@pc15.cs.ucdavis.edu>
2021-05-09 16:16:44 -0700

    100% pass; only 45.7% coverage

--- nfa/nfa.pl
+++ nfa/nfa.pl
@@ -1,3 +1,16 @@
+/* chech if final state is a member of current states */
+reach(SS, FS, []) :- 
+    member(FS,SS).
+
+
+/* check if reachable via next states generated via head and input tail */
+/* if it isnt, continue on the next items of current state */
+reach([SSH|SST], FS, [IH|IT]) :- 
+    transition(SSH,IH,NS),    
+    reach(NS,FS,IT);
+    reach(SST,FS,I). 
+
+
+/* call with start state as a list */
 reachable(StartState, FinalState, Input) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    reach([StartState],FinalState,Input).
